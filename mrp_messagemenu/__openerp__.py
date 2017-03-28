a736b8092c16094a1e0240f14a4f4033484d3e74
Anders Wallenquist <anders.wallenquist@vertel.se>
2017-03-28 13:08:29 +0200

    mrp_messagemenu: some information from event deleted

--- mrp_messagemenu/__openerp__.py
+++ mrp_messagemenu/__openerp__.py
@@ -19,29 +19,15 @@
 #
 ##############################################################################
 {
-<<<<<<< HEAD
-'name': 'Event Message Menu',
-'version': '0.1',
-'summary': 'Menu for event calendar in message menu',
-'category': 'event',
-'description': """Menu for event calendar in message menu
-=======
 'name': 'Manufacturing Message Menu',
 'version': '0.1',
-'summary': 'Menu for event production orders in message menu',
-'category': 'event',
+'summary': 'Menu for production orders in message menu',
+'category': 'mrp',
 'description': """Menu for production calendar in message menu
->>>>>>> 2fa84b6985c6e0c131c12a1e8050a58396716421
-
 """,
 'author': 'Vertel AB',
 'website': 'http://www.vertel.se',
-<<<<<<< HEAD
-'depends': ['event',],
-'data': ['event_messagemenu_view.xml', 'event_messagemenu_data.xml',],
-=======
 'depends': ['mrp',],
 'data': ['mrp_messagemenu_view.xml'],
->>>>>>> 2fa84b6985c6e0c131c12a1e8050a58396716421
 'installable': True,
 }
