122f3d45e01cdb1e6307a6efca8df4b7faf0ea5f
iammiracle <miracleaayodele@gmail.com>
2022-10-06 09:15:34 +0200

    bom report extended

---
+++ mrp_bom_report/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    "name": "MRP BOM Report",
+    "version": "14.0.1.1.0",
+    "category": "Manufacturing",
+    "summary": "Adds more fields to MRP BOM Report",
+    "author": "Vertel AB",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "license": "AGPL-3",
+    "depends": ["mrp_bom_structure_xlsx"],
+    "data": [],
+    "installable": True,
+}
